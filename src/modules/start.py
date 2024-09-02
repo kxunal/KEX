c2d6a081c34db53611033b2f2f6d16cc057d33bf
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2024-09-02 22:32:58 +0530

    Update start.py

--- src/modules/start.py
+++ src/modules/start.py
@@ -12,8 +12,20 @@ from src import app
 @app.on_message(filters.command("start"))
 async def start(_, m: Message):
     add_user_database(m.from_user.id)
-    await m.reply_text(f"""ʜᴇʟʟᴏ {m.from_user.mention}! 👋 ɪ'ᴍ ʏᴏᴜʀ ᴇᴅɪᴛ ɢᴜᴀʀᴅɪᴀɴ ʙᴏᴛ 🤖, ʜᴇʀᴇ ᴛᴏ ᴍᴀɪɴᴛᴀɪɴ ᴀ ꜱᴇᴄᴜʀᴇ ᴇɴᴠɪʀᴏɴᴍᴇɴᴛ ꜰᴏʀ ᴏᴜʀ ᴅɪꜱᴄᴜꜱꜱɪᴏɴꜱ.,\n\n🚫 ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇ ᴅᴇʟᴇᴛɪᴏɴ: ɪ'ʟʟ ʀᴇᴍᴏᴠᴇ ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇꜱ ᴛᴏ ᴍᴀɪɴᴛᴀɪɴ ᴛʀᴀɴꜱᴘᴀʀᴇɴᴄʏ.\n\n📣 ɴᴏᴛɪꜰɪᴄᴀᴛɪᴏɴꜱ: ʏᴏᴜ'ʟʟ ʙᴇ ɪɴꜰᴏʀᴍᴇᴅ ᴇᴀᴄʜ ᴛɪᴍᴇ ᴀ ᴍᴇꜱꜱᴀɢᴇ ɪꜱ ᴅᴇʟᴇᴛᴇᴅ.\n\n🌟 ɢᴇᴛ ꜱᴛᴀʀᴛᴇᴅ:\n: ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ.\n: ᴜꜱᴀɢᴇ: /editmode on/off\n\n➡️ ᴄʟɪᴄᴋ ᴏɴ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴀᴅᴅ ᴍᴇ ᴀɴᴅ ᴋᴇᴇᴘ ᴏᴜʀ ɢʀᴏᴜᴘ ꜱᴀꜰᴇ!"""",
-                         reply_markup=InlineKeyboardMarkup([
-        [InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/{app.me.username}?startgroup=new")],
-        [InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ 🤝", url=f"https://t.me/STORM_TECHH")]
-    ]))
+    await m.reply_text(
+        f"""ʜᴇʟʟᴏ {m.from_user.mention}! 👋 ɪ'ᴍ ʏᴏᴜʀ ᴇᴅɪᴛ ɢᴜᴀʀᴅɪᴀɴ ʙᴏᴛ 🤖, ʜᴇʀᴇ ᴛᴏ ᴍᴀɪɴᴛᴀɪɴ ᴀ ꜱᴇᴄᴜʀᴇ ᴇɴᴠɪʀᴏɴᴍᴇɴᴛ ꜰᴏʀ ᴏᴜʀ ᴅɪꜱᴄᴜꜱꜱɪᴏɴꜱ.,
+        
+🚫 ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇ ᴅᴇʟᴇᴛɪᴏɴ: ɪ'ʟʟ ʀᴇᴍᴏᴠᴇ ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇꜱ ᴛᴏ ᴍᴀɪɴᴛᴀɪɴ ᴛʀᴀɴꜱᴘᴀʀᴇɴᴄʏ.
+        
+📣 ɴᴏᴛɪꜰɪᴄᴀᴛɪᴏɴꜱ: ʏᴏᴜ'ʟʟ ʙᴇ ɪɴꜰᴏʀᴍᴇᴅ ᴇᴀᴄʜ ᴛɪᴍᴇ ᴀ ᴍᴇꜱꜱᴀɢᴇ ɪꜱ ᴅᴇʟᴇᴛᴇᴅ.
+
+🌟 ɢᴇᴛ ꜱᴛᴀʀᴛᴇᴅ:
+: ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
+: ᴜꜱᴀɢᴇ: /editmode on/off
+
+➡️ ᴄʟɪᴄᴋ ᴏɴ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴀᴅᴅ ᴍᴇ ᴀɴᴅ ᴋᴇᴇᴘ ᴏᴜʀ ɢʀᴏᴜᴘ ꜱᴀꜰᴇ!""",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/{app.me.username}?startgroup=new")],
+            [InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ 🤝", url="https://t.me/STORM_TECHH")]
+        ])
+    )
