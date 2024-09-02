d98e7069c648d4397b13e753034622745b8383f7
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2024-09-02 22:07:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Necessary Variables 
-API_ID = int(getenv("API_ID", ))
-API_HASH = getenv("API_HASH", )
-BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 LOG_ID = int(getenv("LOG_ID"))
 SUDOERS = list(map(int, getenv("SUDOERS", "").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
