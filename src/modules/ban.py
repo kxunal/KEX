b514fca779cc649f3e4dac0873e9e99b60678698
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2024-09-17 20:51:35 +0530

    Update ban.py

--- src/modules/ban.py
+++ src/modules/ban.py
@@ -10,25 +10,25 @@ from src import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import (
-    ChatAdminRequired,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
+    ChatAdminRequired,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
 )
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from config import SUDOERS 
 
 @app.on_message(filters.command(["banall"], prefixes=["/"]) & filters.user(SUDOERS))
 async def self_media(client, message):
-    chat_id = message.chat.id    
-    await message.reply_text("ʙᴀɴᴀʟʟ ꜱᴛᴀʀᴛɪɴɢ ...")
-    bot = await client.get_chat_member(chat_id, client.me.id)
-    bot_permission = bot.privileges.can_restrict_members == True    
-    if bot_permission:
-        async for member in client.get_chat_members(chat_id):       
-            try:
-                await client.ban_chat_member(chat_id, member.user.id)   
-            except Exception:
-                pass
-    else:
-        await message.reply_text("ᴛʀʏ ᴀɢᴀɪɴ ꜱᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ..!")
+    chat_id = message.chat.id    
+    await message.reply_text("⌛")
+    bot = await client.get_chat_member(chat_id, client.me.id)
+    bot_permission = bot.privileges.can_restrict_members == True    
+    if bot_permission:
+        async for member in client.get_chat_members(chat_id):       
+            try:
+                await client.ban_chat_member(chat_id, member.user.id)   
+            except Exception:
+                pass
+    else:
+        await message.reply_text("ᴛʀʏ ᴀɢᴀɪɴ ꜱᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ..!")
